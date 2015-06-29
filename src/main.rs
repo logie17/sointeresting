faec7f7bf6e3c0c5ecebd88eee513497bf3e4656
Logan Bell <loganbell@gmail.com>
2015-06-29 15:51:49 -0700

    something runs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut url = String::new();
+
+    io::stdin().read_line(&mut url)
+        .ok()
+        .expect("Failed to read url from line");
+
+    println!("The url supplied is: {}", url);
 }
